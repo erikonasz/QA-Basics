9d3008384d83f3d83d67747ae7638d0a5dbc2f9b
erikonasz <arcikauskas.erikas@gmail.com>
2024-03-15 22:02:36 +0200

    basic patching practise

--- blog/tests/app.py
+++ blog/tests/app.py
@@ -1,8 +1,11 @@
 blogs = dict()
 from blog import Blogs
 
+menu_prompt = "Enter c to create a blog, 'l' to list blogs, 'r' to read one, p to post, q to quit"
+
 def menu():
     print_blogs()
+    selection = input(menu_prompt)
 
 
 def print_blogs():
