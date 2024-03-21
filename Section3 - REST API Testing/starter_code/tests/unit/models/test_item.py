e36d3e7553f7ce038fbba8623765128dac9f08b0
erikonasz <arcikauskas.erikas@gmail.com>
2024-03-21 18:06:59 +0200

    started with second section

---
+++ Section3 - REST API Testing/starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from models.item import ItemModel
+
+class TestItem(TestCase):
+        def test_create_item(self):
+                pass
+        def test_item_json(self):
+                pass
+        
