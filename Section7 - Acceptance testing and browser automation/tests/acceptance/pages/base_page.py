ef28f168e94b296a1ba1816fff20e1519ee33443
erikonasz <arcikauskas.erikas@gmail.com>
2024-04-10 17:03:15 +0300

    Final commit

---
+++ Section7 - Acceptance testing and browser automation/tests/acceptance/pages/base_page.py
@@ -0,0 +1,22 @@
+from tests.acceptance.page_model.base_page import BasePageLocators
+
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @property
+    def url(self):
+        return 'http://127.0.0.1'
+
+    def find_element(self, by, value):
+        return self.driver.find_element(by, value)
+
+    @property
+    def title(self):
+        return self.find_element(*BasePageLocators.TITLE)
+
+    @property
+    def navigation(self):
+        return self.find_elements(*BasePageLocators.NAV_LINKS)
